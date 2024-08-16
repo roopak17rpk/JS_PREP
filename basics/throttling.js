c1120e0570c6ec22f7e7d0f7b1bd2d175d7b85ae
BlkBox Rpk <blkbox@192.168.1.5>
2024-08-16 15:57:13 +0530

    throttling done

--- basics/throttling.js
+++ basics/throttling.js
@@ -1,3 +1,23 @@
 /**
  * its  a simple rate limiting methodology
  */
+
+const throttledFunc = throttle(() => {
+  console.log("throttle up");
+}, 3000);
+
+throttledFunc();
+
+function throttle(fn, limit) {
+  let flag = true;
+  return function (...args) {
+    const context = this;
+    if (flag) {
+      fn.apply(context, args);
+      flag = false;
+      setTimeout(() => {
+        flag = true;
+      }, limit);
+    }
+  };
+}
