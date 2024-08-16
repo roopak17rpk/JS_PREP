7e0a19421f927c914a778fa199088b96bdc45172
BlkBox Rpk <blkbox@192.168.1.5>
2024-08-16 11:36:33 +0530

    cors done

---
+++ basics/cors.js
@@ -0,0 +1,20 @@
+/**
+ * CORS is a mechanism to share resources among two different origin machines
+ * 
+ * Preflight call is made before actual api call is made first
+ * Cors mechanism uses additional HTTP headers to verify this.
+ * 
+ * Machine B verifies these headers and sends back Additional Headers
+ * That Let Machine A Make the Actual Api Call
+ * 
+ * example header : Accept-control-allow-origin : *
+ * this will mean any other domain can have access to resources of 
+ * Machine B. a Public Api or resource 
+ * else a list is set.
+ * 
+ * Machine/Domain A ----Preflight Request----> Machine/Domain B  |
+ * 									<---------Options---------									 |
+ * 																															 | Time
+ * 									---------Api Call--------->									 |
+ * 																															 v
+ */																															
--- js-prep/cors.js
+++
@@ -1,8 +0,0 @@
-/**
- * CORS is a mechanism to share resources among two different origin machines
- * 
- * Preflight call is made before actual api call is made first
- * 
- * 
- * Machine/Domain A  ---Preflight Request----> Machine/Domain B
- */
