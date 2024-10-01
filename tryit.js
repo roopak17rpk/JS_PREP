ef49e75d08aa27416484df42b9708f4529f12459
BlkBox Rpk <blkbox@192.168.1.2>
2024-10-01 14:44:59 +0530

    sync

--- tryit.js
+++ tryit.js
@@ -1,23 +1,28 @@
-function ApiCall() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Promise Resolve");
-    }, 2000);
+Function.prototype.myBind = function (...args) {
+  let context = this;
+  const params = args.slice(1);
+  return function (...localArgs) {
+    context.apply(args[0], [...params, ...localArgs]);
+  };
+};
+
+const myPromiseAll = function (taskList) {
+  let results = [];
+  let completedPromises = 0;
+  return new Promise((resolve, reject) => {
+    taskList.forEach((task) => {
+      task
+        .then((val) => {
+          results[index] = val;
+          completedPromises += 1;
+
+          if (results?.length === taskList?.length) {
+            resolve(results);
+          }
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
   });
-}
-
-async function A() {
-  const res = await ApiCall();
-  console.log("A function called");
-}
-
-function B() {
-  ApiCall().then(() => {
-    console.log("1");
-  });
-  console.log("B function called");
-}
-
-A();
-B();
-console.log("Global call");
+};
