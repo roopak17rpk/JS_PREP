52f1541bed86d125310e7c3017604804838420c6
BlkBox Rpk <blkbox@192.168.1.3>
2024-10-29 19:58:49 +0530

    setTimeout poly done

---
+++ basics/polyfill_setTimeout.js
@@ -0,0 +1,39 @@
+function createTimeout() {
+  let timerId = 0;
+  let timerMap = {};
+
+  const requestIdleCallback = setImmediate;
+
+  function setTimeoutPoly(callback, delay) {
+    id = timerId++;
+    timerMap[id] = true;
+
+    let start = Date.now();
+
+    function triggerCallback(args) {
+      if (!timerMap[id]) return;
+      if (Date.now() - start >= delay) {
+        callback.apply(this, args);
+        delete timerMap[id];
+      } else {
+        requestIdleCallback(triggerCallback);
+      }
+    }
+    requestIdleCallback(triggerCallback);
+    return id;
+  }
+  function clearTimeoutPoly(id) {
+    delete timerMap[id];
+  }
+  return { setTimeoutPoly, clearTimeoutPoly };
+}
+
+const { setTimeoutPoly, clearTimeoutPoly } = createTimeout();
+
+console.log("start");
+const timerID = setTimeoutPoly(() => {
+  console.log("hello");
+}, 2000);
+console.log("end");
+
+// clearTimeoutPoly(timerID);
