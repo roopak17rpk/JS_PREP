5954f18d5eaf0a5c82587b6b7387638514cb0281
BlkBox Rpk <blkbox@192.168.1.3>
2024-10-30 18:46:17 +0530

    repeat string dsa

--- interviewAsked/repeat_string_dsa.js
+++ interviewAsked/repeat_string_dsa.js
@@ -1,4 +1,37 @@
 /**
- *  I was given strings like 2[a2[b]] and 3[b2[ca]]
- *  and asked to return outputs like abbabb and bcacabcacabcaca
+ * Decodes a string with pattern like "2[a2[b]]" -> "abbabb"
+ * @param {string} encodedString - The encoded string to decode
+ * @returns {string} - The decoded string
+ * make better
  */
+const decodeString = (encodedString) => {
+  // Stack to store characters and numbers
+
+  let final = "";
+
+  for (let i = encodedString?.length - 1; i >= 0; i--) {
+    const char = encodedString[i];
+    if (char === "[" || char === "]") {
+      continue;
+    }
+    if (!isNaN(char)) {
+      const num = parseInt(char);
+      // console.log("num", num);
+      final = final.repeat(num);
+      // console.log("final", final);
+      continue;
+    }
+    final = char + final;
+  }
+
+  return final;
+};
+
+// Test cases
+const testCases = ["2[a2[b]]", "3[b2[ca]]"];
+
+testCases.forEach((test) => {
+  const result = decodeString(test);
+  console.log("input_string", test);
+  console.log("decoded_result", result);
+});
