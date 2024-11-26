09712b35c5bbea4b173f5b4bed305ed32b18913f
BlkBox Rpk <blkbox@192.168.1.9>
2024-11-26 11:55:44 +0530

    flatten object

---
+++ basics/flatten_objects.js
@@ -0,0 +1,46 @@
+const FlattenObj = (obj) => {
+  let finalObj = {};
+
+  function generateFlattenObj(obj, parent) {
+    for (let key in obj) {
+      const newParent = parent + key;
+      const value = obj[key];
+      if (typeof value === "object") {
+        generateFlattenObj(value, newParent + ".");
+      } else {
+        finalObj[newParent] = value;
+      }
+    }
+  }
+  generateFlattenObj(obj, "");
+  return finalObj;
+};
+
+const nestedObject = {
+  name: "John",
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    country: {
+      code: "US",
+      name: "United States",
+    },
+  },
+  hobbies: ["reading", "traveling"],
+  education: {
+    highSchool: {
+      name: "Anytown High",
+      graduationYear: 2005,
+    },
+    university: {
+      name: "State University",
+      graduationYear: 2009,
+      degree: "Computer Science",
+    },
+  },
+};
+
+const flattenObject = FlattenObj(nestedObject);
+
+console.log("flattenObject", flattenObject);
+console.log("flattenObject", flattenObject[`hobbies.0`]);
